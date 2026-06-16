085500471a5ca2253e6d6f306b2cf8e7d29751df
Yugraj Mangate <Yugraj.Mangate@cyberxdelta.com>
2026-06-16 15:26:44 +0530

    Support root base path for Vercel custom domain and subpath for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // <--- THIS IS CRITICAL. It must match your GitHub Repo name.
+  base: "/", // Default base path for Vercel and local development
 })
